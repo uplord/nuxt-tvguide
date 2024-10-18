1a2b7be7b9fe9a300ef75bcb54bf7235026ca8d3
Michael <michael@uplord.co.uk>
2024-10-18 23:01:34 +0100

    nuxt image amend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   css: ['~/assets/scss/globals.scss'],
   devtools: { enabled: true },
+  image: {
+    domains: ['msaas.img.freeviewplay.net'],
+  },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image'],
   srcDir: "src/",
   ssr: true,
