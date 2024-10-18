38beb6a4bee6469ccff3a3ae1451a1129585ff05
Michael <michael@uplord.co.uk>
2024-10-18 22:37:11 +0100

    center clear button

--- src/components/Channels.vue
+++ src/components/Channels.vue
@@ -15,9 +15,11 @@
           />
           <button
             @click="clearSearch"
-            class="ml-2 p-3 bg-red-500 text-white rounded hover:bg-red-600"
+            class="flex items-center justify-center ml-2 p-3 bg-blue-500 text-white rounded hover:bg-blue-600 w-[50px]"
           >
-            Clear
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
           </button>
         </div>
 
