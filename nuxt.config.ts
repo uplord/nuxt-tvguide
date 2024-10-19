1355811ee512f9084560a8f1bfcbae6d06c3f9f2
Michael <michael@uplord.co.uk>
2024-10-19 10:28:02 +0100

    image domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   css: ['~/assets/scss/globals.scss'],
   devtools: { enabled: true },
   image: {
-    domains: ['msaas.img.freeviewplay.net'],
+    domains: ['msaas.img.freeviewplay.net', 'fdp-sv15-image-v1-0.gcprod1.freetime-platform.net'],
   },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image'],
   srcDir: "src/",
